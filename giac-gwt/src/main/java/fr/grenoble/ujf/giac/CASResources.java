ab7362052ad36235fe3e8491b104c6f82fa0f38f
Zoltán Kovács <zoltan@geogebra.org>
2023-03-31 08:50:23 +0200

    Revert to old version for GeoGebra Discovery

--- giac-gwt/src/main/java/fr/grenoble/ujf/giac/CASResources.java
+++ giac-gwt/src/main/java/fr/grenoble/ujf/giac/CASResources.java
@@ -1,15 +1,33 @@
 package fr.grenoble.ujf.giac;
 
+
+/* new version (incompatible with GeoGebra Discovery, FIXME)
 import org.gwtproject.resources.client.ClientBundle;
 import org.gwtproject.resources.client.Resource;
 import org.gwtproject.resources.client.TextResource;
+*/
+
+/* old version */
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.TextResource;
+
+
 
 
 /**
  * CAS resource bundle
  */
+
+/* new version
 @Resource
+*/
 public interface CASResources extends ClientBundle {
+
+	/**
+	 * maybe it's better if INSTANCE is created later?
+	 */
+	CASResources INSTANCE = GWT.create(CASResources.class);
 
 	/** @return giac.wasm */
 	@Source("fr/grenoble/ujf/giac/giac.wasm.js")
